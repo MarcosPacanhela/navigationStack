7be07bd95a748018af373fc2eb7d3815437675e2
Marcos Pacanhela <marcosalexandrepacanhela71@gmail.com>
2024-08-11 17:31:59 -0300

    Aplicando conceitos de navegação Stack

---
+++ src/screens/GalleryScreen.tsx
@@ -0,0 +1,11 @@
+import {StatusBar} from 'expo-status-bar';
+import {View, Text} from 'react-native';
+
+export default function GalleryScreen(){
+    return (
+        <View style={{flex:1, backgroundColor: 'blue'}}>
+            <Text>Gallery works !!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
