fa88c9b2fc2c3cf35a1229e4b2c61f331ab2440f
Marcos Pacanhela <marcosalexandrepacanhela71@gmail.com>
2024-08-12 08:17:23 -0300

    Aplicando conceitos de navegação Drawer navigation

---
+++ src/routes/drawer.routes.tsx
@@ -0,0 +1,26 @@
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import HomeScreen from '../screens/HomeScreen'
+import GalleryScreen from '../screens/GalleryScreen'
+
+const { Navigator, Screen } = createDrawerNavigator()
+
+export function DrawerRoutes() {
+    return (
+        <Navigator>
+            <Screen
+                name='home'
+                component={HomeScreen}
+                options={{
+                    title: 'Home'
+                }}
+            />
+            <Screen
+                name='gallery'
+                component={GalleryScreen}
+                options={{
+                    title: 'Gallery'
+                }}
+            />
+        </Navigator>
+    )
+}
